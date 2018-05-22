8f31d4ac99097bba4feef8ea863f21f14a53c81b
Levarrishawk <levarrishawk@gmail.com>
2018-05-22 05:09:19 -0400

    [fixed] Indentation build erros.

--- MMOCoreORB/src/server/zone/objects/creature/commands/SurpriseShotCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SurpriseShotCommand.h
@@ -20,11 +20,11 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
-			ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
-			CreatureObject* targetCreature = dynamic_cast<CreatureObject*>(object.get());
+		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
+		CreatureObject* targetCreature = dynamic_cast<CreatureObject*>(object.get());
 
-			if (targetCreature == NULL)
-				return INVALIDTARGET;
+		if (targetCreature == NULL)
+			return INVALIDTARGET;
 
 		return SUCCESS;
 	}
