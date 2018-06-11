836a40238d12521f59d6af67e56d86a6b9f57991
Levarrishawk <levarrishawk@gmail.com>
2018-06-11 04:59:14 -0400

    [iteration] Moraband

--- MMOCoreORB/bin/scripts/screenplays/cities/moraband_valley.lua
+++ MMOCoreORB/bin/scripts/screenplays/cities/moraband_valley.lua
@@ -10,6 +10,7 @@ function MorabandValleyScreenPlay:start()
 	if (isZoneEnabled("moraband")) then
 		self:spawnMobiles()
 		self:spawnSceneObjects()
+		self:spawnActiveArea1()
 	end
 end
 
@@ -40,4 +41,73 @@ function MorabandValleyScreenPlay:spawnMobiles()
   end
 
   
+end
+
+function MorabandValleyScreenPlay:spawnActiveArea1()
+  local pSpawnArea1 = spawnSceneObject("moraband", "object/active_area.iff", -1319, 14, -585, 0, 0)
+    
+  if (pSpawnArea1 ~= nil) then
+    local activeArea1 = LuaActiveArea(pSpawnArea1)
+          activeArea1:setCellObjectID(0)
+          activeArea1:setRadius(32)
+          createObserver(ENTEREDAREA, "MorabandValleyScreenPlay", "notifySpawnArea1", pSpawnArea1)
+          --createObserver(EXITEDAREA, "MorabandValleyScreenPlay", "notifySpawnAreaLeave", pSpawnArea1)
+      end
+end
+
+function MorabandValleyScreenPlay:notifySpawnArea1(pActiveArea1, pMovingObject, pPlayer)
+  
+  if (not SceneObject(pMovingObject):isCreatureObject()) then
+    return 0
+  end
+  
+  return ObjectManager.withCreatureObject(pMovingObject, function(player)
+    if (player:isAiAgent()) then
+      return 0
+    end
+    
+    if ((player:isImperial() or player:isRebel()or player:isNeutral())) then
+
+      player:sendSystemMessage("You feel cold as you approach the edifice of the Tomb.")
+      player:playMusicMessage("sound/music_event_danger.snd")
+      
+      end
+    return 0    
+  end)
+end
+
+function MorabandValleyScreenPlay:spawnActiveArea2()
+  local pSpawnArea2 = spawnSceneObject("moraband", "object/active_area.iff", -1102, 93, -484, 0, 42000220)
+    
+  if (pSpawnArea2 ~= nil) then
+    local activeArea2 = LuaActiveArea(pSpawnArea2)
+          activeArea2:setCellObjectID(42000220)
+          activeArea2:setRadius(512)
+          createObserver(ENTEREDAREA, "MorabandValleyScreenPlay", "notifySpawnArea2", pSpawnArea2)
+          --createObserver(EXITEDAREA, "MorabandValleyScreenPlay", "notifySpawnAreaLeave", pSpawnArea1)
+      end
+end
+
+function MorabandValleyScreenPlay:notifySpawnArea2(pActiveArea2, pMovingObject, pPlayer)
+  
+  if (not SceneObject(pMovingObject):isCreatureObject()) then
+    return 0
+  end
+  
+  return ObjectManager.withCreatureObject(pMovingObject, function(player)
+    if (player:isAiAgent()) then
+      return 0
+    end
+    
+    if (( player:isRebel()or player:isNeutral() )) then
+
+      player:sendSystemMessage("You feel uneasy as you enter the building.  The Dark Side is strong here.")
+      player:playMusicMessage("sound/music_event_danger.snd")
+    end  
+    if (( player:isImperial() )) then
+      player:sendSystemMessage("You feel a rush of energy.  The Dark Side is strong here.")
+      player:playMusicMessage("sound/music_become_dark_jedi.snd")
+    end
+    return 0    
+  end)
 end
