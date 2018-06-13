3438e5aa8c90a827742d3474bbae8e5e17f4e3e0
Levarrishawk <levarrishawk@gmail.com>
2018-06-13 05:03:55 -0400

    [iteration] Resources on Hutta / Moraband

--- MMOCoreORB/src/server/zone/managers/resource/ResourceManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/resource/ResourceManagerImplementation.cpp
@@ -158,6 +158,8 @@ void ResourceManagerImplementation::loadDefaultConfig() {
 	resourceSpawner->addZone("endor");
 	resourceSpawner->addZone("chandrila");
 	resourceSpawner->addZone("kaas");
+	resourceSpawner->addZone("moraband");
+	resourceSpawner->addZone("hutta");
 
 	shiftInterval = 7200000;
 	resourceSpawner->setSpawningParameters(1, 86400, 90, 1000, 0);
