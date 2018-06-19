7fa345998eb582f72ed223efcd30dddd3d59511e
Levarrishawk <levarrishawk@gmail.com>
2018-06-19 04:09:09 -0400

    [iteration] Moraband Spawns

--- MMOCoreORB/bin/scripts/mobile/spawn/moraband_world.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/moraband_world.lua
@@ -99,7 +99,61 @@ moraband_world = {
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
-		},		
+		},
+		{
+          lairTemplateName = "dantooine_dark_side_savage_neutral_none",
+          spawnLimit = -1,
+          minDifficulty = 75,
+          maxDifficulty = 79,
+          numberToSpawn = 0,
+          weighting = 15,
+          size = 40
+        },
+        {
+          lairTemplateName = "dantooine_force_renegade_neutral_none",
+          spawnLimit = -1,
+          minDifficulty = 80,
+          maxDifficulty = 84,
+          numberToSpawn = 0,
+          weighting = 15,
+          size = 40
+        },
+        {
+          lairTemplateName = "dantooine_force_archaist_neutral_none",
+          spawnLimit = -1,
+          minDifficulty = 90,
+          maxDifficulty = 94,
+          numberToSpawn = 0,
+          weighting = 15,
+          size = 40
+        },
+        {
+          lairTemplateName = "dantooine_force_crypt_crawler_neutral_none",
+          spawnLimit = -1,
+          minDifficulty = 95,
+          maxDifficulty = 99,
+          numberToSpawn = 0,
+          weighting = 15,
+          size = 40
+        },
+        {
+          lairTemplateName = "dantooine_force_wilder_neutral_none",
+          spawnLimit = -1,
+          minDifficulty = 115,
+          maxDifficulty = 119,
+          numberToSpawn = 0,
+          weighting = 15,
+          size = 40
+        },
+        {
+          lairTemplateName = "dantooine_force_crystal_hunter_neutral_none",
+          spawnLimit = -1,
+          minDifficulty = 115,
+          maxDifficulty = 119,
+          numberToSpawn = 0,
+          weighting = 15,
+          size = 40
+        },		
 	}
 }
 
