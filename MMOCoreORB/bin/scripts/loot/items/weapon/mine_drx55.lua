fd1d6cf3b1fcddb3c503b60cc60e11cd68b7fe82
Levarrishawk <levarrishawk@gmail.com>
2018-06-19 04:12:09 -0400

    [fixed] Typo

--- MMOCoreORB/bin/scripts/loot/items/weapon/mine_drx55.lua
+++ MMOCoreORB/bin/scripts/loot/items/weapon/mine_drx55.lua
@@ -22,7 +22,7 @@ mine_drx55 = {
 	-- randomDotChance: The chance of this weapon object dropping with a random dot on it. Higher number means less chance. Set to 0 to always have a random dot.
 	randomDotChance = 625,
 	junkDealerTypeNeeded = JUNKARMS,
-	junkMiValue = 25,
+	junkMinValue = 25,
 	junkMaxValue = 45
 }
 
