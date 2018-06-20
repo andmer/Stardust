5e4d796648d4cf3507fa472d940530cfc5a11495
Levarrishawk <levarrishawk@gmail.com>
2018-06-20 03:33:27 -0400

    [iteration] Shyrack Cave

--- MMOCoreORB/bin/scripts/screenplays/cities/moraband_valley.lua
+++ MMOCoreORB/bin/scripts/screenplays/cities/moraband_valley.lua
@@ -187,6 +187,93 @@ function MorabandValleyScreenPlay:spawnMobiles()
     pNpc = spawnMobile("moraband", "commoner", 60, 1101, 18, -5311, -173, 0)
     self:setMoodString(pNpc, "neutral")
  
+ 
+    -- Shyrack Cave
+    spawnMobile("moraband", "terentatek", 60, -58, -57.8, -231, -5, 42000085)
+    spawnMobile("moraband", "terentatek", 60, -80.8, -58.4, -187.4, 52, 42000084)
+    spawnMobile("moraband", "terentatek", 60, -49.6, -58.8, -182.1, -52, 42000084)
+    
+    spawnMobile("moraband", "shyrack", 60, -64.5, -58.2, -139, 0, 42000081)
+    spawnMobile("moraband", "shyrack", 60, -48.5, -61, -126, 0, 42000081)
+    
+    spawnMobile("moraband", "shyrack", 60, -22.3, -60.1, -109.7, 87, 42000078) -- Lower level (multi tiered cell)
+    
+    spawnMobile("moraband", "shyrack", 60, 3.4, -63.4, -107.1, -36, 42000082)
+    
+    spawnMobile("moraband", "shyrack", 60, 4.5, -59.4, -68.9, 90, 42000076) -- Lower level (multi tiered cell)
+    
+    spawnMobile("moraband", "shyrack", 60, 39.3, -58.3, -86.7, -46, 42000083)
+    spawnMobile("moraband", "shyrack", 60, 30.8, -59.2, -90.2, -5, 42000083)
+    spawnMobile("moraband", "shyrack", 60, 49, -57.6, -85.6, -63, 42000083)
+    spawnMobile("moraband", "shyrack", 60, 46, -59.3, -67.9, -89, 42000083)
+    
+    spawnMobile("moraband", "shyrack", 60, -77, -63.5, -106, -174, 42000065) -- Lower level (multi tiered cell)
+    spawnMobile("moraband", "shyrack", 60, -92.6, -62.8, -120.8, -14, 42000065)
+    spawnMobile("moraband", "shyrack", 60, -111.3, -61.4, -104.9, 121, 42000065)
+    
+    spawnMobile("moraband", "shyrack", 60, -107.8, -58.8, -137, -101, 42000080)
+    spawnMobile("moraband", "shyrack", 60, -124.4, -52.5, -138.4, -23, 42000080)
+    
+    spawnMobile("moraband", "shyrack", 60, -128.8, -45.8, -112.3, 89, 42000079)
+    
+    spawnMobile("moraband", "shyrack", 60, -52.7, -41.6, -103.5, 69, 42000078) -- Upper Level (multi tiered cell)
+    spawnMobile("moraband", "shyrack", 60, -27.2, -36.2, -99, -7, 42000078)
+    
+    spawnMobile("moraband", "shyrack", 60, -20.1, -27.1, -68.9, 94, 42000076) -- Upper level (multi tiered cell)
+    spawnMobile("moraband", "shyrack", 60, 18.5, -27.3, -75.3, -11, 42000076)
+    
+    spawnMobile("moraband", "tukata", 60, -21, -21.1, -26.7, -1, 42000058)
+    spawnMobile("moraband", "tukata", 60, -28.6, -22.5, -20.2, 78, 42000058)
+    spawnMobile("moraband", "tukata", 60, -39.6, -24.7, -23.2, 0, 42000058)
+    spawnMobile("moraband", "tukata", 60, -8.6, -22.5, -46, -91, 42000058)
+    
+    spawnMobile("moraband", "tukata", 60, -43.8, -21.8, -47.1, 40, 42000059)
+    spawnMobile("moraband", "tukata", 60, -52.3, -21.8, -40.8, 105, 42000059)
+    
+    spawnMobile("moraband", "tukata", 60, -72.6, -22.5, -45.6, 71, 42000061)
+    spawnMobile("moraband", "tukata", 60, -66.8, -22.8, -64.9, 7, 42000061)
+    
+    spawnMobile("moraband", "tukata", 60, -73, -30.1, -113.3, -95, 42000065) -- Top Level (Multi tiered cell)
+    spawnMobile("moraband", "tukata", 60, -86.8, -29, -122.4, 83, 42000065)
+    spawnMobile("moraband", "tukata", 60, -104, -30.7, -106.1, 165, 42000065)
+    
+    spawnMobile("moraband", "tukata", 60, -104, -30.9, -84, 177, 42000067) -- Top level (multi tiered cell)
+    spawnMobile("moraband", "tukata", 60, -127.3, -30.1, -81.4, 87, 42000067)
+    
+    spawnMobile("moraband", "tukata", 60, -125.9, -25.9, -63.6, 86, 42000062) -- Top level (multi tiered cell)
+    spawnMobile("moraband", "tukata", 60, -105.3, -28.1, -65.2, -88, 42000062)
+    
+    spawnMobile("moraband", "tukata", 60, -103.9, -34.2, -30, 177, 42000072) -- Upper level   
+    spawnMobile("moraband", "tukata", 60, -113.6, -50.4, -31, -78, 42000072) -- Lower level
+    
+    spawnMobile("moraband", "tukata", 60, -119.3, -38.6, -8.7, 98, 42000073)
+    
+    spawnMobile("moraband", "tukata", 60, -80, -53.8, -61.3, 172, 42000070)
+    
+    spawnMobile("moraband", "tukata", 60, -102.3, -47.2, -81.2, -92, 42000067) -- Lower level (multi tiered cell)
+    spawnMobile("moraband", "tukata", 60, -115, -46.7, -88.3, -92, 42000067)
+    spawnMobile("moraband", "tukata", 60, -123.3, -45.8, -82.1, -87, 42000067)
+    spawnMobile("moraband", "tukata", 60, -136.6, -46.2, -89.2, -40, 42000067)
+    spawnMobile("moraband", "tukata", 60, -143.3, -46, -82, 135, 42000067)
+    spawnMobile("moraband", "tukata", 60, -144.6, -45.5, -114.6, 0, 42000067)
+    spawnMobile("moraband", "tukata", 60, -163.5, -48.1, -121.6, 92, 42000067)
+    spawnMobile("moraband", "tukata", 60, -176.4, -44, -102.2, 173, 42000067)   
+    spawnMobile("moraband", "tukata", 60, -174.5, -41.1, -77.4, 179, 42000067)
+    
+    spawnMobile("moraband", "tukata", 60, -169.7, -41.7, -87.2, -175, 42000066)
+    spawnMobile("moraband", "tukata", 60, -164.9, -41.4, -58.6, -175, 42000066)
+    
+    spawnMobile("moraband", "tukata", 60, -151.1, -43, -41.6, 89, 42000064)
+    
+    spawnMobile("moraband", "tukata", 60, -127.8, -41.4, -65, -68, 42000062)
+    spawnMobile("moraband", "tukata", 60, -116.3, -41.4, -57.4, -104, 42000062)
+    spawnMobile("moraband", "tukata", 60, -97.3, -43.7, -60.5, -88, 42000062)
+    
+    spawnMobile("moraband", "tukata", 60, -71, -38.6, -57.7, 44, 42000061)
+    
+    spawnMobile("moraband", "tukata", 60, -64.7, -37, -17.2, 70, 42000060)
+    
+    
    end
 
   
