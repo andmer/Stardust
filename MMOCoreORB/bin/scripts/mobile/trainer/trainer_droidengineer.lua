85467474c5a991d270ba6a3e314bfa81e0ac5da8
Levarrishawk <levarrishawk@gmail.com>
2018-07-24 02:01:10 -0400

    [hotfix] 1.0.05a - fixed one additional trainer name

--- MMOCoreORB/bin/scripts/mobile/trainer/trainer_droidengineer.lua
+++ MMOCoreORB/bin/scripts/mobile/trainer/trainer_droidengineer.lua
@@ -1,5 +1,6 @@
 trainer_droidengineer = Creature:new {
-	objectName = "@mob/creature_names:trainer_droidengineer",
+	objectName = "",
+	customName = "a Droid Engineer trainer",
 	randomNameType = NAME_GENERIC,
 	randomNameTag = true,
 	faction = "",
