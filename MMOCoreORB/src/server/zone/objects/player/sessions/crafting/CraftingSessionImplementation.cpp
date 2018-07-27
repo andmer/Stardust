63fa45499395f69795ccecbe222f07f617f04f2e
Levarrishawk <levarrishawk@gmail.com>
2018-07-27 19:48:28 -0400

    [hotfix] 1.0.12 - Adjusted Practice Crafting XP Gain
    Practice Mode crafting will award twice the XP of creating the physical
    item.

--- MMOCoreORB/src/server/zone/objects/player/sessions/crafting/CraftingSessionImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/player/sessions/crafting/CraftingSessionImplementation.cpp
@@ -1185,7 +1185,7 @@ void CraftingSessionImplementation::createPrototype(int clientCounter, bool crea
 
 			// This is for practicing
 			startCreationTasks(manufactureSchematic->getComplexity() * 2, true);
-			xp = round(xp * 1.05f);
+			xp = round(xp * 2.0f);
 		}
 
 		Reference<PlayerManager*> playerManager = crafter->getZoneServer()->getPlayerManager();
