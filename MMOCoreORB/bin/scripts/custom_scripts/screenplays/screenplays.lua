b6d05bccb83f24fe7ec54d716797eeaade7bbedd
Levarrishawk <levarrishawk@gmail.com>
2018-07-30 01:36:48 -0400

    [iteration] First Stage: Combat XP Conversion NPC
    The foundations to creating an NPC that will allow players to
    interchange their combat XP at a cost.

--- MMOCoreORB/bin/scripts/custom_scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/custom_scripts/screenplays/screenplays.lua
@@ -1,3 +1,4 @@
 -- Battle of Endor Temporary Screenplays.  Disable after Timeline change.
 
---includeFile("../custom_scripts/screenplays/battle_of_endor/endor_phase_1.lua")
+includeFile("../custom_scripts/screenplays/respec_counselor/respec_conv_handler.lua")
+includeFile("../custom_scripts/screenplays/respec_counselor/convohelpers/experience_converter.lua")
