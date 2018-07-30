21ee961d2d12a608c5c27290625a51183facc3d8
Levarrishawk <levarrishawk@gmail.com>
2018-07-30 02:55:15 -0400

    [hotfix] 1.0.17b

--- MMOCoreORB/src/server/zone/objects/building/components/EnclaveContainerComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/building/components/EnclaveContainerComponent.cpp
@@ -6,6 +6,8 @@
 #include "server/zone/objects/intangible/PetControlDevice.h"
 #include "server/zone/objects/intangible/tasks/PetControlDeviceStoreObjectTask.h"
 #include "server/zone/managers/frs/FrsManager.h"
+#include "server/zone/ZoneServer.h"
+#include "server/zone/Zone.h"
 
 bool EnclaveContainerComponent::checkContainerPermission(SceneObject* sceneObject, CreatureObject* creature, uint16 permission) const {
 	if (sceneObject->isBuildingObject())
@@ -49,7 +51,7 @@ bool EnclaveContainerComponent::checkBuildingPermission(SceneObject* sceneObject
 	if (ghost->hasGodMode())
 		return true;
 
-	if (ghost->getZoneName() != "yavin4")
+	if (!ghost->getZone()->getZoneName() == "yavin4")
 		return true;
 
 	int enclaveType = 0;
@@ -90,7 +92,7 @@ bool EnclaveContainerComponent::checkCellPermission(SceneObject* sceneObject, Cr
 	if (ghost->hasGodMode())
 		return true;
 
-	if (ghost->getZoneName() != "yavin4")
+	if (!ghost->getZone()->getZoneName() != "yavin4")
 			return true;
 
 	int enclaveType = 0;
