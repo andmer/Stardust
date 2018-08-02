7cde22dca2b57f8d05c6a9e2ed2f8ee3a6a06614
Levarrishawk <levarrishawk@gmail.com>
2018-08-01 23:06:41 -0400

    [hotfix] 1.1.09 - Fixed Dot Weapon Loot
    Some weapons were dropping with 100% chance of a random Dot.   This was
    not intended.

--- MMOCoreORB/bin/scripts/loot/items/weapon/nyax_sword.lua
+++ MMOCoreORB/bin/scripts/loot/items/weapon/nyax_sword.lua
@@ -28,7 +28,7 @@ nyax_sword = {
 	randomDotChance = 600,
 
 	-- staticDotChance: The chance of this weapon object dropping with a static dot on it. Higher number means less chance. Set to 0 to always have a static dot.
-	staticDotChance = 0,
+	staticDotChance = 400,
 
 	-- staticDotType: 1 = Poison, 2 = Disease, 3 = Fire, 4 = Bleed
 	staticDotType = 1,
